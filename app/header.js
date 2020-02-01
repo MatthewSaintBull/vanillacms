d53f556d7fe0a85ab1988ce2675bf4f925e79aa9
Matteo Santoro <mattinter997@gmail.com>
2020-02-01 09:48:34 +0100

    installed axios + nodemon + cors-express + moved up pages hardocded data from client to server + implemented nodemon in script on package.json

--- app/header.js
+++ app/header.js
@@ -1,15 +1,19 @@
 function loadNavbar() { //loads the navbar with custom links given by the server
-    //const pages = fetch from server the pages
-    for (let page of pages) {
-        const item = document.createElement('LI')
-        item.classList.add("nav-item")
-        const itemContent = document.createElement('a')
-        itemContent.classList.add("nav-link")
-        const itemText = document.createTextNode(page)
-        itemContent.appendChild(itemText)
-        itemContent.addEventListener("click", (event) => { loadContent(event).then((res) => { routeTo(res.obj); res.event.target.classList.add('active') }) })
-        item.appendChild(itemContent)
-        console.log(item)
-        navbar.appendChild(item)
-    }
+    axios.get('http://localhost:3000/pages').then(
+        res=>{
+            console.log("pages : ", res.data.pages)
+            for (let page of res.data.pages) {
+                const item = document.createElement('LI')
+                item.classList.add("nav-item")
+                const itemContent = document.createElement('a')
+                itemContent.classList.add("nav-link")
+                const itemText = document.createTextNode(page)
+                itemContent.appendChild(itemText)
+                itemContent.addEventListener("click", (event) => { loadContent(event).then((res) => { routeTo(res.obj); res.event.target.classList.add('active') }) })
+                item.appendChild(itemContent)
+                console.log(item)
+                navbar.appendChild(item)
+            }
+        }
+    )
 }
--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+var cors = require('cors')
+app.use(cors())
 
-app.get('/', (req, res) => res.send('It Works!'))
+
+
+const pages = ["home", "test-fetch", "contattaci"] //those pages are a mock for what that will be the server in the future
+
+app.get('/pages', (req, res) => res.send({pages}))
 
 app.listen(port, () => console.log(`server started on port ${port}!`))
