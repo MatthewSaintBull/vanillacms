0ef80a027c4fe81c8ecd9e07fc403c6ec00c6030
Matteo Santoro <mattinter997@gmail.com>
2020-02-02 17:13:19 +0100

    moved content mock onto server + pipeline analisi

--- app/index.js
+++ app/index.js
@@ -6,23 +6,7 @@ const selectorContent = document.querySelector("#content") //id for the content 
 window.addEventListener('hashchange', renderContent); //observable for the route change
 
 
-const content = [ //those contents are a mock for what that will be the server in the future
-    {
-        'route': 'home',
-        'page': 'home',
-        'content': `<h3>home</h3>`
-    },
-    {
-        'route': 'chi-sono',
-        'page': 'chi sono',
-        'content': `<h3>chi sono</h3>`
-    },
-    {
-        'route': 'contattaci',
-        'page': 'contattaci',
-        'content': `<h3>contattaci</h3>`
-    }
-]
+
 
 window.dispatchEvent(new Event('hashchange')) //trigger of the observable for the first route check
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+
+const _ = require('lodash')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -6,9 +8,37 @@ var cors = require('cors')
 app.use(cors())
 
 
+const content = [ //those contents are a mock for what that will be the server in the future
+    {
+        'id': '1',
+        'route': 'home',
+        'page': 'home',
+        'content': `<h3>home</h3>`
+    },
+    {
+        'id': '2',
+        'route': 'chi-sono',
+        'page': 'chi sono',
+        'content': `<h3>chi sono</h3>`
+    },
+    {
+        'id': '3',
+        'route': 'contattaci',
+        'page': 'contattaci',
+        'content': `<h3>contattaci</h3>`
+    }
+]
+
+
+const handleContent = (req) => {
+    return _.findLast(content, function(o) {
+        return o.id===req.params.contentID
+    }) || false // put error message for handling
+}
 
 const pages = ["home", "test-fetch", "contattaci"] //those pages are a mock for what that will be the server in the future
 
 app.get('/pages', (req, res) => res.send({pages}))
+app.get('/content/:contentID', (req, res) => res.send(handleContent(req)))
 
 app.listen(port, () => console.log(`server started on port ${port}!`))
